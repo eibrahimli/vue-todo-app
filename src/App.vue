fd1546875b538cb88bf0c67268d5846292ca8c04
unknown <ibrahimlielvir@gmail.com>
2020-08-06 13:50:33 +0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app" class="app">
+    <img alt="Vue logo" src="./assets/logo.png" class="logo">
+    <todo-list></todo-list>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList'
+
+export default {
+  name: 'App',
+  components: {
+    TodoList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.app{
+  max-width: 600px;
+  margin: 20px auto;
+}
+.logo{
+  display: block;
+  margin: 20px auto;
+  height: 75px;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <input type="text" class="todo-input" placeholder="What needs to be done">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='scss'>
+  .todo-input{    
+    
+    width: 100%;
+    padding: 16px 18px;
+    font-size: 18px;
+    margin-bottom: 16px;
+
+    &:focus{
+      outline: none;
+    }
+  }
+</style>
